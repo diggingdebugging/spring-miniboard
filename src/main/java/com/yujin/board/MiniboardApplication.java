1591bd4cee94b2300b99f622e7d03d2066e545d9
yujin <yujin3289@naver.com>
2025-04-25 17:00:02 +0900

    init: 스프링 미니 게시판 프로젝트 초기 설정

---
+++ src/main/java/com/yujin/board/MiniboardApplication.java
@@ -0,0 +1,11 @@
+package com.yujin.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniboardApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MiniboardApplication.class, args);
+	}
+}
---
+++ src/test/java/com/yujin/board/MiniboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yujin.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
