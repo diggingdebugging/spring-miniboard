ce905cebf22e2500804f6b51e28f53ab243aedca
yujin <yujin3289@naver.com>
2025-04-27 02:37:03 +0900

    feat: MemoryPostRepository CRUD 기능 구현

--- src/main/java/com/yujin/board/domain/repository/MemoryPostRepository.java
+++ src/main/java/com/yujin/board/domain/repository/MemoryPostRepository.java
@@ -3,6 +3,7 @@ package com.yujin.board.domain.repository;
 import com.yujin.board.domain.Post;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -10,30 +11,36 @@ import java.util.Map;
 @Repository
 public class MemoryPostRepository implements PostRepository {
     private final Map<Long, Post> store = new HashMap<>();
-    private long sequence = 0L;
+    private long sequence = 0L; //새로운 게시물에 적용되는 아이디 값, 게시물이 생성될 때 save에서 1씩증가.
 
+    //create
     @Override
     public Post save(Post post) {
-        return null;
+        post.setId(sequence++);
+        store.put(post.getId(), post);
+        return post;
     }
 
+    //read
     @Override
     public Post findById(Long id) {
-        return null;
+        return store.get(id);
     }
 
     @Override
     public List<Post> findAll() {
-        return null;
+        return new ArrayList<>(store.values());
     }
 
     @Override
     public void update(Long id, Post updatedPost) {
-
+        Post post = findById(id);
+        post.setTitle(updatedPost.getTitle());
+        post.setContent(updatedPost.getContent());
     }
 
     @Override
     public void delete(Long id) {
-
+        store.remove(id);
     }
 }
