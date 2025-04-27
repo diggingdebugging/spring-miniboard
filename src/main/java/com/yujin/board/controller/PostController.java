c676ed26c028c39ab9be4982a0a2f8cb5746e662
yujin <yujin3289@naver.com>
2025-04-28 02:08:33 +0900

    feat: PostController /new post요청 처리하는 save메소드 구현

--- src/main/java/com/yujin/board/controller/PostController.java
+++ src/main/java/com/yujin/board/controller/PostController.java
@@ -1,10 +1,13 @@
 package com.yujin.board.controller;
 
+import com.yujin.board.domain.Post;
 import com.yujin.board.domain.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
@@ -23,5 +26,11 @@ public class PostController {
     @GetMapping("/new")
     public String createForm() {
         return "post/postForm";
+    }
+
+    @PostMapping("/new")
+    public String save(@ModelAttribute Post post) {
+        postRepository.save(post);
+        return "redirect:/posts";
     }
 }
