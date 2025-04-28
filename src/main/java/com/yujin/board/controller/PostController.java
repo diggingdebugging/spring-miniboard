7b1f795a09a38ec3d1aecdd7e9b69bcd96a2be81
yujin <yujin3289@naver.com>
2025-04-28 15:40:40 +0900

    feat: postEdit 화면 추가 및 PostController editForm메서드 추가

--- src/main/java/com/yujin/board/controller/PostController.java
+++ src/main/java/com/yujin/board/controller/PostController.java
@@ -17,7 +17,7 @@ import java.util.List;
 public class PostController {
     private final PostRepository postRepository;
 
-    // 메인 화면
+    // 메인 화면 요청
     @GetMapping
     public String list(Model model) {
         List<Post> posts = postRepository.findAll();
@@ -25,21 +25,32 @@ public class PostController {
         return "/post/postList";
     }
 
+    // 게시물 등록 화면 요청
     @GetMapping("/new")
     public String createForm() {
         return "post/postForm";
     }
 
+    // 게시물 등록 후 변경사항 저장 및 리다이렉트
     @PostMapping("/new")
     public String save(@ModelAttribute Post post) {
         postRepository.save(post);
         return "redirect:/posts";
     }
 
+    // 게시물 세부사항 요청
     @GetMapping("/{id}")
     public String post(@PathVariable Long id, Model model) {
         Post post = postRepository.findById(id);
         model.addAttribute("post", post);
         return "post/postDetail";
+    }
+
+    // 게시물 수정 요청
+    @GetMapping("/{id}/edit")
+    public String editForm(@PathVariable Long id, Model model) {
+        Post post = postRepository.findById(id);
+        model.addAttribute("post", post);
+        return "post/postEdit";
     }
 }
